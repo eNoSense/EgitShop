1dd085055876acd4739eb3240d42f8cd74de029b
HongKunWang <lt13127042108@163.com>
2015-06-24 22:00:08 +0800

    DAO commit

---
+++ src/bean/dao/GoodsDAO.java
@@ -0,0 +1,43 @@
+package bean.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import bean.DBBean;
+import bean.vo.GoodsVo;
+
+public class GoodsDAO {
+	
+	int numberPage = 2;
+	
+	public ArrayList getGoodsByPage(int pageNo){
+		ArrayList goodsList =  null;
+		
+		DBBean db = new DBBean();
+		Connection con = db.getConnection();
+		ResultSet rs = null;
+		
+		try {
+			rs = db.exectueQuery("select * from goods limit "+(pageNo-1)*2+","+numberPage, null);
+			
+			if(rs!=null){
+				goodsList = new ArrayList();
+				while(rs.next()){
+					GoodsVo g = new GoodsVo();
+					g.setGoodsId(rs.getString("goodsid"));
+					g.setGoodsName(rs.getString("goodsname"));
+					g.setPrice(rs.getFloat("price"));
+					goodsList.add(g);
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return goodsList;
+	}
+
+}
---
+++ src/bean/vo/GoodsVo.java
@@ -0,0 +1,29 @@
+package bean.vo;
+
+public class GoodsVo {
+	private String GoodsId;
+	private String GoodsName;
+	private Float price;
+	
+	public String getGoodsId() {
+		return GoodsId;
+	}
+	public void setGoodsId(String goodsId) {
+		GoodsId = goodsId;
+	}
+	public String getGoodsName() {
+		return GoodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		GoodsName = goodsName;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	
+
+}

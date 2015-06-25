2197edb7546c907e8b672a3ef731d58b5ef8d1c0
HongKunWang <lt13127042108@163.com>
2015-06-25 11:10:46 +0800

    AddToCartServlet

---
+++ src/servlets/AddToCart.java
@@ -0,0 +1,91 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.dao.CartManager;
+
+public class AddToCart extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public AddToCart() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//设置输入输出编码格式
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		//输入处理
+		String goodsId = request.getParameter("goodsId");
+		//业务逻辑
+		HttpSession session = request.getSession();
+		ArrayList cart = (ArrayList) session.getAttribute("session");
+		
+		CartManager cartM = new CartManager();
+		
+		if(cart != null){
+			cartM.setCart(cart);
+		}
+		
+		cartM.addToCart(goodsId, 1);
+		
+		session.setAttribute("cart",cartM.getCart());
+		//处理输出
+		response.sendRedirect("cart.jsp");
+
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
